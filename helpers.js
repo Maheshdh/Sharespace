6d7481522337691be1ee10d3a31b6ff545ad800a
Maheshdh <mahesh.dhondge@gmail.com>
2023-04-29 00:30:44 -0400

    Added createListing function in data/listings.js; Added checkDate function in helpers.js

--- helpers.js
+++ helpers.js
@@ -53,6 +53,34 @@ const exportedMethods = {
         if (phone_no.match(phoneRegEx)) {
           return phone_no 
         } else throw `Error: ${varName} entered is not valid`
+      },
+
+      checkDate(date,varName){
+        // First check for the pattern
+        if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(date))
+        throw `Error: You must enter a valid ${varName}!`;
+        // Parse the date parts to integers
+        var parts = date.split("/");
+        var day = parseInt(parts[1], 10);
+        var month = parseInt(parts[0], 10);
+        var year = parseInt(parts[2], 10);
+
+        // Check the ranges of month and year
+        if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        throw `Error: You must enter a valid ${varName}!`;
+
+        var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+        // Adjust for leap years
+        if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+            monthLength[1] = 29;
+
+        // Check the range of the day
+        if(!(day > 0 && day <= monthLength[month - 1])){
+          throw `Error: You must enter a valid ${varName}!`;
+
+        }
+        return date;
       }
 
 
