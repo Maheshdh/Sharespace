38320ee0660868525bfe6e4e0fe5daa870d04de2
Maheshdh <mahesh.dhondge@gmail.com>
2023-04-29 18:26:25 -0400

    added functions for deleteListing and modifyListing

--- data/listings.js
+++ data/listings.js
@@ -15,7 +15,7 @@ export const createListing = async (
     listing_AvailableEndInput
 ) => {
 
-    if (!address || !description || !price || !length || !width || !height|| !pictures|| !latitude|| !longitute|| !availability) throw 'Error: Invalid number of parameters entered (Expected 5)'
+    if (!address || !description || !price || !length || !width || !height|| !pictures|| !latitude|| !longitute|| !listing_AvailableStartInput || listing_AvailableEndInput) throw 'Error: Invalid number of parameters entered (Expected 5)'
     if (!address) throw 'Error: "address" parameter not entered'
     if (!description) throw 'Error: "description" parameter not entered'
     if (!price) throw 'Error: "price" parameter not entered'
@@ -63,11 +63,78 @@ export const createListing = async (
 
 export const deleteListing = async (
     listingID
-) => {}
+) => {
+    listingID = helpers.checkId(listingID);
+    const listingsCollection = await listings();
+    const deletionInfo = await listingsCollection.findOneAndDelete({
+      _id: new ObjectId(listingID)
+    });
+    if (deletionInfo.lastErrorObject.n === 0)
+      throw [404, `Could not delete listing with id of ${listingID}`];
+    return {...deletionInfo.value, deleted: true};
+}
 
 export const modifyListing = async (
-    listingID
-) => {}
+    listingID,
+    address,
+    description,
+    price,
+    length,
+    width,
+    height,
+    latitude,
+    longitute,
+    listing_AvailableStartInput,
+    listing_AvailableEndInput
+) => {
+
+    if (!address || !description || !price || !length || !width || !height|| !pictures|| !latitude|| !longitute|| !availability) throw 'Error: Invalid number of parameters entered (Expected 5)'
+    if (!address) throw 'Error: "address" parameter not entered'
+    if (!description) throw 'Error: "description" parameter not entered'
+    if (!price) throw 'Error: "price" parameter not entered'
+    if (!length) throw 'Error: "length" parameter not entered'
+    if (!width) throw 'Error: "width" parameter not entered'
+    if (!height) throw 'Error: "height" parameter not entered'
+    if (!latitude) throw 'Error: "latitude" parameter not entered'
+    if (!longitute) throw 'Error: "longitute" parameter not entered'
+    if (!listing_AvailableStartInput) throw 'Error: "listing_AvailableStartInput" parameter not entered'
+    if (!listing_AvailableEndInput) throw 'Error: "listing_AvailableEndInput" parameter not entered'
+   
+    listingID = helpers.checkId(listingID, 'listingID') 
+    address = helpers.checkString(address, 'address')
+    description = helpers.checkString(description, 'description')
+    price = helpers.checkPrice(price, 'price')
+    length = helpers.checkDimensions(length, 'length')
+    width = helpers.checkDimensions(width, 'width')
+    height = helpers.checkDimensions(height, 'height')
+    listing_AvailableStartInput = helpers.checkDate(listing_AvailableStartInput,"Start Date")
+    listing_AvailableEndInput = helpers.checkDate(listing_AvailableEndInput,"End Date")
+
+    let updatedListing = {
+        address : address,
+        description : description,
+        price : price,
+        length : length,
+        width : width,
+        height : height,
+        volume : volume,
+        latitude : latitude,
+        longitute : longitute,
+        listing_AvailableStartInput : listing_AvailableStartInput,
+        listing_AvailableEndInput : listing_AvailableEndInput
+  
+      };
+      let listingsCollection = await listings()
+      const updateInfo = await listingsCollection.findOneAndReplace(
+        {_id: new ObjectId(listingID)},
+        updatedListing,
+        {returnDocument: 'after'}
+      );
+      if (updateInfo.lastErrorObject.n === 0)
+        throw [404, `Error: Update failed! Could not update listing with id ${listingID}`];
+      return updateInfo.value;
+
+}
 
 export const getAllListings = async() => {
     const listingsCollection = await listings();
