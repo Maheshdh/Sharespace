17d68cb9d377ddfdcf00cd4b682f4a774e22639b
Sanchit Sahdev <sanchitsahdev2@gmail.com>
2023-05-01 19:35:01 -0400

    Ratings done with MAJOR database changes

--- data/users.js
+++ data/users.js
@@ -2,6 +2,8 @@ import { ObjectId } from "mongodb"
 import {users} from '../config/mongoCollections.js'
 import helpers from '../helpers.js'
 import bcrypt from 'bcrypt'
+import { getListing } from "./listings.js";
+import { getReview } from "./reviews.js";
 
 export const createUser = async (
     firstName,
@@ -37,6 +39,7 @@ export const createUser = async (
       phoneNumber: phoneNumber,
       listings: [],
       rating: 0,
+      reviews: [],
       role: role
     }
     
@@ -68,6 +71,7 @@ export const checkUser = async (emailAddress, password) => {
         phoneNumber: currentUser.phoneNumber,
         listings: currentUser.listings,
         rating: currentUser.rating,
+        reviews: currentUser.reviews,
         role: currentUser.role
       }
       if (await bcrypt.compare(password, currentUser.password)) return (currentUserInfo)
@@ -76,9 +80,62 @@ export const checkUser = async (emailAddress, password) => {
   };
 
 export const getUser = async (id) => {
-    id = helpers.checkId(id);
+    id = helpers.checkId(id, 'User ID');
     const userCollection = await users();
     const user = await userCollection.findOne({_id: new ObjectId(id)});
     if(!user) throw "The user is not found";
     return user;
   };
+
+
+export const updateUserRatingAndComments = async (id) => {
+    id = helpers.checkId(id, "User ID");
+    let user = await getUser(id)
+    let userListingsIDs = user.listings //Array of strings where each string represents LISTING ID
+  
+    let userListingsInfo = [] //Contains INFORMATION of all listings made by User
+    if (userListingsIDs.length == 0) return 'User has no active listings!'
+    for (let listingID of userListingsIDs) {
+      userListingsInfo.push(await getListing(listingID))
+    }
+
+    let reviewsOnUserIDs = [] //Array of strings where each string represents the REVIEW ID's made on EVERY listing made by user
+    for (let listingInfo of userListingsInfo) {
+      let allReviewsOnListing = listingInfo.reviews //Array of strings where each string represents REVIEW ID
+      for (let reviewID of allReviewsOnListing) {
+        reviewsOnUserIDs.push(reviewID)
+      }
+    }
+
+    let allRatings = [] //Array of numbers where each number represents every RATING given to every listings made by user
+    let allReviews = [] //Array of strings where each string represents every COMMENT made on every listings made by user
+    for (let reviewID of reviewsOnUserIDs) {
+      let reviewInfo = await getReview(reviewID)
+      allRatings.push(reviewInfo.rating)
+      let review = {
+        reviewMadeBy: reviewInfo.userID, //This is the ID of the user who MADE the review/comment
+        rating: reviewInfo.rating,
+        comment: reviewInfo.comment
+      }
+      allReviews.push(review)
+    }
+
+    //Finding the Rating of the user by averaging 'allRatings'
+    let totalSum = 0
+    let count = 0
+    for (let rating of allRatings) {
+      totalSum += rating
+    }
+    let userRating = totalSum/(allRatings.length)
+    let userRatingRoundedUp = Math.round(userRating*100)/100
+
+    let userCollection = await users()
+    console.log('Received user collection')
+    let updateUser = await userCollection.findOneAndUpdate(
+      {_id: new ObjectId(id)},
+      {$set: {rating: userRatingRoundedUp, reviews:allReviews}}
+    )
+    console.log('Updated user collection')
+
+    return true
+}
